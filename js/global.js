da82cea1627c7a46b401db4e7ab9ac22ebb3fca0
Matt <destroyer273@hotmail.co.uk>
2020-10-16 23:17:07 +0100

    Added image modal, and floating next button

--- js/global.js
+++ js/global.js
@@ -5,19 +5,29 @@ function toggleMenu() {
   if (menuOpen == true) {
     closeNav();
     menuOpen = false;
-    console.log('Close menu');
+    console.log('Close menu'); //Test ------------
   } else {
     openNav();
     menuOpen = true;
-    console.log('Open');
+    console.log('Open menu'); //Test ------------
   }
 }
 function openNav() {
-  document.getElementById('mySidenav').style.width = '250px';
+  document.getElementById('sideNav').style.width = '250px';
 }
 
 function closeNav() {
-  document.getElementById('mySidenav').style.width = '0';
+  document.getElementById('sideNav').style.width = '0';
 }
 
-/////
+//Image modal display
+
+function openModal(img, pageModal, modalImg) {
+  console.log('Open image modal'); //Test ------------
+  pageModal.style.display = 'block';
+  modalImg.src = img.src;
+  caption.innerHTML = img.alt;
+}
+function closeModal(modal) {
+  modal.style.display = 'none';
+}
